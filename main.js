4e839278d2190dbceb7ebf31234bc8d3f7fbad68
Alisa Ono <alisao@mit.edu>
2018-04-19 19:46:14 -0400

    Populate menu popup

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   $('body').on('click', function(e){
     if (e.target.className !== 'menu-item' && e.target.className !== 'menu-popup') {
-      $(`.menu-popup`).hide()
+      $('.menu-popup').hide()
+      $('.menu-subpopup').hide()
     }
   })
 
-  $('#nav-bar .menu-item').on('click', function(){
+  $('#nav-bar > .menu-item-wrapper > .menu-item').on('click', function(){
     let $popup = $(this).parent().children('.menu-popup')
     let hidden = $popup.css('display') === 'none'
-    $(`.menu-popup`).hide()
+    $('.menu-popup').hide()
+    $('.menu-subpopup').hide()
     if (hidden) {
       $popup.show()
     }
+  })
+
+  $('#current-matches').on('click', function(){
+    $(this).children('.menu-subpopup').show()
   })
 })
